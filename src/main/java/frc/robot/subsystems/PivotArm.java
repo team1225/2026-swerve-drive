7b7c661813e819bdc70ff221e1ec1bda22849834
Jason Heilemann <traeh@aesthetics.tail312e8.ts.net>
2026-01-24 17:57:53 -0500

    completed vendor dependency issues - code builds in 2026 IDE now

--- src/main/java/frc/robot/subsystems/PivotArm.java
+++ src/main/java/frc/robot/subsystems/PivotArm.java
@@ -6,7 +6,8 @@ package frc.robot.subsystems;
 
 import com.revrobotics.spark.SparkBase.PersistMode;
 import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+import com.revrobotics.spark.config.ClosedLoopConfig;
+import com.revrobotics.spark.FeedbackSensor;
 import com.revrobotics.spark.config.MAXMotionConfig.MAXMotionPositionMode;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
